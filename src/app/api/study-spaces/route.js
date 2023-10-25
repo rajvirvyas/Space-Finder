7aaf095c90603c2a5debc98f4f3679a5b412004b
sreshtalluri <sreshta.talluri@gmail.com>
2023-10-24 20:24:46 -0700

    work on schema and POST request

--- src/app/api/study-spaces/route.js
+++ src/app/api/study-spaces/route.js
@@ -11,12 +11,15 @@ export async function POST(request) {
     const loggedInData = await checkLoggedIn();
     if (loggedInData.loggedIn) {
         // add the exact attributes of the schema and update
-        const { done, value } = await request.json();
+        const { name, building, long, lat, cap, } = await request.json();
         const studyRoom = await prisma.studySpace.create({
         data: {
             ownerId: loggedInData.user?.id,
-            done,
-            value
+            name: name,
+            building: building,
+            longitude: long,
+            latitude: lat,
+            capacity: cap
         }
         });
         return NextResponse.json(studyRoom);
