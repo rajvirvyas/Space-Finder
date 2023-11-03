8890357c063b5140bb1753d158e0d679cf7a968f
sreshtalluri <sreshta.talluri@gmail.com>
2023-11-03 09:47:41 -0700

    routes for users

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,16 +1,30 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/db";
 import bcrypt from 'bcryptjs';
+import { checkLoggedIn } from "@/lib/auth";
+
+export async function GET(request) {
+  const loggedInData = await checkLoggedIn();
+  if (loggedInData.loggedIn) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: loggedInData.user?.id
+      }
+    });
+    return NextResponse.json(user);
+  }
+  return NextResponse.json({error: 'not signed in'}, {status: 403});
+}
 
 export async function POST(request) {
   const data = await request.json();
-  const { email, password } = data;
-  if (email && password) {
+  const { username, email, password } = data;
+  if (username && email && password) {
     const hashedPassword = await bcrypt.hash(password, 10);
     let user;
     try {
       user = await prisma.user.create({
-        data: { email, password: hashedPassword }
+        data: { username, email, password: hashedPassword }
       });
     } catch (e) {
       return NextResponse.json({error: e.message}, {status: 500 })
