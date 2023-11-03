cc84199876ab821179c6ea64a242fef18c8f0108
sreshtalluri <sreshta.talluri@gmail.com>
2023-11-02 19:44:26 -0700

    added routes for study spaces

--- src/app/api/study-spaces/route.js
+++ src/app/api/study-spaces/route.js
@@ -3,23 +3,22 @@ import prisma from "@/lib/db";
 import { checkLoggedIn } from "@/lib/auth";
 
 export async function GET(request) {
-  const studySpaces = await prisma.studySpace.findMany();
+  const studySpaces = await prisma.studySpace.findMany({});
   return NextResponse.json(studySpaces);
 }
 
 export async function POST(request) {
     const loggedInData = await checkLoggedIn();
     if (loggedInData.loggedIn) {
-        // add the exact attributes of the schema and update
-        const { name, building, long, lat, cap, } = await request.json();
+        const { name, building, longitude, latitude, capacity } = await request.json();
         const studyRoom = await prisma.studySpace.create({
         data: {
             ownerId: loggedInData.user?.id,
-            name: name,
-            building: building,
-            longitude: long,
-            latitude: lat,
-            capacity: cap
+            name,
+            building,
+            longitude,
+            latitude,
+            capacity
         }
         });
         return NextResponse.json(studyRoom);
