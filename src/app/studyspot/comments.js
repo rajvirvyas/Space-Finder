d4d327e9acdef3d015e699442e8717d0df974fb7
SOMATUMMALA21 <somatummala03@gmail.com>
2023-11-08 12:35:22 -0800

    [Issue 54] Add a page to see specific information regarding a study space/room
    Needs to be linked fom individual card components in the home page

---
+++ src/app/studyspot/comments.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { Box, Button } from '@mui/material';
+
+export default function Comments() {
+  return (
+    <>
+        <Box component="span" display="flex" flexDirection="column">
+            <Box component="span" m={1}>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                    <Avatar alt="Musty Mustang" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                    primary="Great place to study"
+                    secondary={
+                        <React.Fragment>
+                        <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                        >
+                            Musty Mustang
+                        </Typography>
+                        {" — Lovely location for studying. Not so lovely for eating brunch."}
+                        </React.Fragment>
+                    }
+                    />
+                </ListItem>
+                <Divider variant="inset" component="li" />
+                <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                    <Avatar alt="Busty Mustang" src="/static/images/avatar/2.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                    primary="Not bad, not great"
+                    secondary={
+                        <React.Fragment>
+                        <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                        >
+                            Busty Mustang
+                        </Typography>
+                        {" — Great place to study. Not many lights tho."}
+                        </React.Fragment>
+                    }
+                    />
+                </ListItem>
+                <Divider variant="inset" component="li" />
+                <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                    <Avatar alt="Mr. Mustang" src="/static/images/avatar/3.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                    primary="AWESOME PLACE TO STUDY"
+                    secondary={
+                        <React.Fragment>
+                        <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                        >
+                            Mr. Mustang
+                        </Typography>
+                        {' — A great place to be focused'}
+                        </React.Fragment>
+                    }
+                    />
+                </ListItem>
+                </List>
+            </Box>
+            <TextField
+                id="standard-textarea"
+                label="Add a Comment"
+                placeholder="Placeholder"
+                multiline
+                variant="standard"
+            />
+            <Button variant="contained">Submit</Button>
+            
+        </Box>
+    </>
+  );
+}
