50b6d8d5df7025e5aef7eb202d68bb7b7869ed84
sreshtalluri <sreshta.talluri@gmail.com>
2023-11-09 16:39:40 -0800

    routes for comments and adding study space to database

--- src/app/AddSpot.js
+++ src/app/AddSpot.js
@@ -25,12 +25,42 @@ export default function AddSpot() {
     setOpen(false);
   }
 
+  function handleAddSpot(event) {
+    event.preventDefault();
+    let valid  = event.currentTarget.reportValidity();
+    const data = new FormData(event.currentTarget);
+    if (valid) {
+      const spotData = {};
+      spotData['name'] = data.get('spotName');
+      spotData['building'] = data.get('building');
+      spotData['capacity'] = +data.get('capacity');
+      console.log(spotData);
+      // submit form
+      fetch("/api/study-spaces", {
+        method: 'post',
+        body: JSON.stringify(spotData)
+      }).then((res) => {
+        if (res.ok) {
+          setOpen(false);
+          setFormState({});
+          setError(false);
+          res.json().then((j) => console.log('success:' + j));
+        } else {
+          setError(true);
+          res.json().then((j) => console.log('error:' + j));
+        }
+      })
+    } else {
+      setFormState({...formState})
+    }
+  }
+
   return (
     <>
       <Button variant="underlined" color="inherit" onClick={handleAddButton}>Add Spot</Button>
       {open && <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Add Study Spot</DialogTitle>
-        
+        <form onSubmit={handleAddSpot}>
         <DialogContent>
           <DialogContentText>
             To Add a new spot, please fill in the following fields.
@@ -44,25 +74,24 @@ export default function AddSpot() {
             id="spotName"
             name="spotName"
             label="Spot Name"
-            type="spotName"
+            type="text"
             fullWidth
             variant="standard"
             required
-     
           />
           <TextField
             margin="dense"
             id="building"
             name="building"
             label="Building"
-            type="building"
+            type="text"
             required
             fullWidth
             variant='standard'/>
           <TextField
             margin="dense"
             name="capacity"
-            id="SpotCap"
+            id="capacity"
             label="Spot Capacity"
             inputProps={{min: 0}}
             type="number"
@@ -74,7 +103,7 @@ export default function AddSpot() {
           <Button onClick={handleClose}>Cancel</Button>
           <Button type="submit">Submit for Review</Button>
         </DialogActions>
-        
+        </form>
       </Dialog>}
     </>
   );
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -58,6 +58,5 @@ export default function Profile() {
             </Box>
         </Box>
         </>
-        
     )
 }
