d2442377ca4fca508eee4c4f7fb9a7e683e29f1a
Rajvir Vyas <rajvirvyas23@gmail.com>
2023-12-14 16:59:44 -0800

    fixed merge conflict with soma's code

--- src/app/components/comments.js
+++ src/app/components/comments.js
@@ -5,123 +5,46 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-
-import { Box } from '@mui/material';
-
-export default function Comments({ reviews }) {
-  const renderReviews = (reviewsArray) => {
-    return reviewsArray.map((review, index) => 
-    (
-
 import Typography from '@mui/material/Typography';
 import { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import { Box, Button, IconButton } from '@mui/material';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import { useSession } from 'next-auth/react';
-
-export default function Comments() {
-    const { data: session, status } = useSession();
-    const [comments, setComments] = useState([]);
-    const [commentText, setCommentText] = useState('');
 
 
-  const handleCommentChange = (event) => {
-    setCommentText(event.target.value);
-  };
-
-  const handleCommentSubmit = () => {
-    if (commentText.trim() !== '') {
-      const newComment = {
-        text: commentText,
-        rating: 0,
-        voted: null, 
-      };
-
-      setComments([...comments, newComment]);
-      setCommentText('');
-    }
-  };
-
-  const handleVote = (index, type) => {
-    const updatedComments = [...comments];
-  
-    if (type === 'up') {
-      if (updatedComments[index].voted === 'up') {
-        updatedComments[index].voted = null;
-        updatedComments[index].rating -= 1;
-      } else if (updatedComments[index].voted === 'down') {
-        updatedComments[index].rating += 2;
-        updatedComments[index].voted = 'up';
-      } else {
-        updatedComments[index].rating += 1;
-        updatedComments[index].voted = 'up';
-      }
-    } else if (type === 'down') {
-      if (updatedComments[index].voted === 'down') {
-
-        updatedComments[index].voted = null;
-        updatedComments[index].rating += 1;
-      } else if (updatedComments[index].voted === 'up') {
-    
-        updatedComments[index].rating -= 2;
-        updatedComments[index].voted = 'down';
-      } else {
-        
-        updatedComments[index].rating -= 1;
-        updatedComments[index].voted = 'down';
-      }
-    }
-  
-    setComments(updatedComments);
-  };
-
-  const renderComments = (commentsArray) => {
-    return commentsArray.map((comment, index) => (
-
-      <>
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt={`User${review.userId + 1}`} src={`/static/images/avatar/${review.userId + 1}.jpg`} />
-          </ListItemAvatar>
-          <ListItemText
-            primary={review.comment}
-            secondary={`— User ${review.userId + 1} | Rating: ${review.value}`}
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-      </>
-    ));
-  }
-
-  return (
-    <>
-      <Box component="span" display="flex" flexDirection="column">
-        <Box component="span" m={1}>
-          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            {renderReviews(reviews)}
-          </List>
-        </Box>
-
-        <TextField
-          id="standard-textarea"
-          label="Add a Comment"
-          placeholder="Comment"
-          multiline
-          variant="standard"
-          value={commentText}
-          onChange={handleCommentChange}
+export default function Comments({ reviews }) {
+  const renderReviews = (reviewsArray) => {
+    return reviewsArray.map((review, index) => 
+    (<>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt={`User${review.userId + 1}`} src={`/static/images/avatar/${review.userId + 1}.jpg`} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={review.comment}
+          secondary={`— User ${review.userId + 1} | Rating: ${review.value}`}
         />
-        {status === "authenticated"?
-        <Button variant="contained" onClick={handleCommentSubmit}>
-          Submit
-        </Button> :
-        <Button disabled variant="contained" onClick={handleCommentSubmit}>
-        Submit
-      </Button>}
-
-      </Box>
+      </ListItem>
+      <Divider variant="inset" component="li" />
     </>
-  );
+  ));
 }
+
+return (
+  <>
+    <Box component="span" display="flex" flexDirection="column">
+      <Box component="span" m={1}>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          {renderReviews(reviews)}
+        </List>
+      </Box>
+    </Box>
+  </>
+);
+}
+
+
+
+
+
