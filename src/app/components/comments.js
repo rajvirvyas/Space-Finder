9258a84c75c8921bd0a2da9d41959be424017170
Alex Warda <alexsio03@gmail.com>
2023-12-14 21:09:21 -0800

    ratings show name

--- src/app/components/comments.js
+++ src/app/components/comments.js
@@ -5,17 +5,18 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import { useState, useEffect } from 'react';
-import TextField from '@mui/material/TextField';
-import { Box, Button, IconButton } from '@mui/material';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import { Box, Typography } from '@mui/material';
 
 
-export default function Comments({ reviews }) {
-  const renderReviews = (reviewsArray) => {
-    return reviewsArray.map((review, index) => 
+export default function Comments({ reviews, users }) {
+
+return (
+  <>
+    <Box component="span" display="flex" flexDirection="column">
+      <Box component="span" m={1}>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        {users.length != 0 ? 
+          reviews.map((review, index) => 
     (<>
       <ListItem alignItems="flex-start">
         <ListItemAvatar>
@@ -23,20 +24,12 @@ export default function Comments({ reviews }) {
         </ListItemAvatar>
         <ListItemText
           primary={review.comment}
-          secondary={`— User ${review.userId + 1} | Rating: ${review.value}`}
+          secondary={`— ${users[index]?.name} | Rating: ${review.value}`}
         />
       </ListItem>
       <Divider variant="inset" component="li" />
     </>
-  ));
-}
-
-return (
-  <>
-    <Box component="span" display="flex" flexDirection="column">
-      <Box component="span" m={1}>
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          {renderReviews(reviews)}
+  )) : <Typography>Loading...</Typography>}
         </List>
       </Box>
     </Box>
